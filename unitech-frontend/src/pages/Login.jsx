33b1ae8436cf3c784b355f20943731216f66a8b5
Laye Soma Doumbia <102613188+Yella90@users.noreply.github.com>
2026-06-18 22:29:18 -1200

    Update Login.jsx

--- unitech-frontend/src/pages/Login.jsx
+++ unitech-frontend/src/pages/Login.jsx
@@ -21,7 +21,7 @@ function Login() {
     setLoginPortal('admin');
 
     try {
-      const response = await fetch(`${backendUrl}/api/auth/login`, {
+      const response = await fetch(`api/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
